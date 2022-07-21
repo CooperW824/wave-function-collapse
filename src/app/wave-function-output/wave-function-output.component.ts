a34741f0976663898e6fc8ba6962170391ac66d7
Cooper Werner <cooperwerner824@gmail.com>
2022-07-21 14:18:16 -0400

    Documentation Update
    Adresses issue Documentation #6

--- src/app/wave-function-output/wave-function-output.component.ts
+++ src/app/wave-function-output/wave-function-output.component.ts
@@ -12,12 +12,17 @@ type Tile = TileObj;
 })
 
 export class WaveFunctionOutputComponent implements OnInit {
-	dim = 10;
+	dim = 20;
 	cells: Cell[][] = Array.from(Array(this.dim), () => new Array(this.dim))
 	tiles: Tile[] = [];
 	failed:boolean = false;
 
 	constructor() { }
+
+
+	/**
+	 * Ran by angular on page load, generates the list of tiles and initializes the cells array with CellObj objects
+	 */
 
 	ngOnInit(): void {
 		this.tiles.push(new TileObj("/assets/tiles/basicTee/tile1.png", ["ABA", "ABA",'AAA','ABA'], 0));
@@ -30,7 +35,14 @@ export class WaveFunctionOutputComponent implements OnInit {
 		}
 	}
 
-	rotateArray(arr:string[]):string[]{
+	/**
+	 * Shifts every element of the array arr 1 position to the left without
+	 * modifying the original array.
+	 * @param arr the array to rotate
+	 * @returns arr with each element shifted 1 position to the left
+	 */
+	// TODO: needs refactoring to fit with clean code standards
+	shiftArrayLeft(arr:string[]):string[]{
 		var outputArr = []
 		outputArr.push(arr[3])
 		outputArr.push(arr[0])
@@ -39,14 +51,27 @@ export class WaveFunctionOutputComponent implements OnInit {
 		return outputArr;
 	}
 
+	/**
+	 * Appends the tile array with each rotation of the tile passed as a parameter.
+	 * @param tile the Tile object to be rotated in all other orientations.
+	 */
+
 	rotateTile(tile: Tile): void{
 		var sides:string[] = tile.getSideKeys();
 		var img = tile.image;
 		for(var r = 1; r < 4; r++){
-			sides = this.rotateArray(sides);
+			sides = this.shiftArrayLeft(sides);
 			this.tiles.push(new TileObj(img,sides, r*90));
 		}
 	}
+
+	/**
+	 * Generates a list of tiles from candidates that are valid with the adjecent cell in the specified direction
+	 * @param candidates The array of possible tiles the current cell could collapse to.
+	 * @param adjTile The tile of the adjacent collapsed cell in the specified direction
+	 * @param direction The direction of the adjacent collapsed cell that the function is comparing against
+	 * @returns a Tile[] that contains all the valid tiles in the specified direction that the current cell could collapse to. 
+	 */
 	
 	checkValid(candidates: Tile[], adjTile:Tile, direction:number): Tile[]{
 		var otherDir = (direction+2)%4;
@@ -61,6 +86,13 @@ export class WaveFunctionOutputComponent implements OnInit {
 		return output;
 	}
 
+	/**
+	 * Collapses the cell at the specified location. Selects which tile to display,
+	 * marks the cell as collapsed, and renders the picture.
+	 * @param r the row of the cell to be collapsed
+	 * @param c the collumn of the cell to be collapsed
+	 */
+
 	collapse(r: number, c: number): void{
 		var cell = this.cells[r][c];
 		cell.setCollapsed(true);
@@ -70,6 +102,13 @@ export class WaveFunctionOutputComponent implements OnInit {
 		this.cells[r][c] = cell;
 		this.render(r,c,tile.image,tile.getRotation());
 	}
+
+	/**
+	 * Gets the valid tiles of the specified cell in all directions, 
+	 * updates the valid tiles and entropy values of the specified cell.
+	 * @param r the row of the cell to be updated
+	 * @param c the collumn of the cell to be updated
+	 */
 
 	updateCell(r:number, c: number): void{
 		var currentCell = this.cells[r][c];
@@ -106,15 +145,24 @@ export class WaveFunctionOutputComponent implements OnInit {
 		this.cells[r][c] = currentCell;
 	}
 
+	/**
+	 * Resets all cells valid tiles and entropy values to the defaults 
+	 * and renders a default black img to the page
+	 */
+
 	reset():void{
 		for(var r= 0; r < this.cells.length; r++){
 			for(var c=0; c<this.cells[r].length; c++){
 				this.cells[r][c].setCollapsed(false);
 				this.cells[r][c].setValidTiles(this.tiles);
-				this.render(r,c,"../../assets/tiles/basicTee/blank.png",0);
+				this.render(r,c,"../../assets/tiles/basicTee/blank.png",0); //This is obsolete at this point, remove at the optimization pass
 			}
 		}
 	}
+
+	/**
+	 * Resets all the cells and runs the generate function, called by the "Generate!" button onClick event.
+	 */
 
 	run():void{
 		this.reset();
@@ -124,6 +172,12 @@ export class WaveFunctionOutputComponent implements OnInit {
 			this.generate(0,0)
 		}
 	}
+
+	/**
+	 * Recursively generates the board, the heart of the WFC algorithm
+	 * @param r the row of the cell to collapse and evaluate surrounding entropy
+	 * @param c the collumn of the cell to collapse and evaluate surrounding entropy
+	 */
 
 	generate(r:number, c:number): void{
 		var lowestEntropy = 5;
@@ -190,6 +244,14 @@ export class WaveFunctionOutputComponent implements OnInit {
 		
 		this.generate(lowestEntropyCoords[0], lowestEntropyCoords[1])
 	}
+
+	/**
+	 * Renders the desired tile img to the screen and rotates it to the desired angle.
+	 * @param r the row of the img element to render to
+	 * @param c the collumn of the img element to render to
+	 * @param tile the path to the image to be rendered
+	 * @param rotation the desired rotation of the img element (degrees)
+	 */
 
 	render(r:number, c:number, tile:string, rotation:number): void{
 		let imgId = "img"+r+" "+c;
